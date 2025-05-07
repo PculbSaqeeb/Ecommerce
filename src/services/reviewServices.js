837d5d8b51ca5a7c0e9409307fd48da0b7e8aeb7
vipulkmit <vipul.kmit@gmail.com>
2025-05-07 18:07:33 +0530

    remove console

--- src/services/reviewServices.js
+++ src/services/reviewServices.js
@@ -3,14 +3,6 @@ import axiosInstance from "../utils/interceptor";
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const reviewEndpoint = (productId,rating,comment,images) => {
-    // console.log(productId);
-    // console.log(rating);
-    // console.log(comment);
-    // console.log(images);
-    // console.log(typeof productId);
-    // console.log(typeof rating);
-    // console.log(typeof comment);
-    // console.log(typeof images);
     return axiosInstance.post(`${BASE_URL}review/${productId}`, {
         rating:rating,
         comment:comment,
